34ff92595fc86797683ea2a1e23b26bcb19e0edf
trevoirwilliams <braniac22@hotmail.com>
2024-03-15 13:20:46 -0500

    Completed Containerization of API

--- ConferenceAttendees.Api/Program.cs
+++ ConferenceAttendees.Api/Program.cs
@@ -17,11 +17,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.Migrate();
-}
+//using (var scope = app.Services.CreateScope())
+//{
+//    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+//    db.Database.Migrate();
+//}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
